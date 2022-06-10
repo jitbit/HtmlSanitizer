d93315b551a87e96c0213c06d1decf1c37b4f2c2
Alexander Yumashev <33555768+alex-jitbit@users.noreply.github.com>
2022-06-10 16:41:44 +0300

    refactored variable names

--- HtmlSanitizer.js
+++ HtmlSanitizer.js
@@ -6,21 +6,21 @@
 
 var HtmlSanitizer = new (function () {
 
-	var tagWhitelist_ = {
+	var _tagWhitelist = {
 		'A': true, 'ABBR': true, 'B': true, 'BLOCKQUOTE': true, 'BODY': true, 'BR': true, 'CENTER': true, 'CODE': true, 'DD': true, 'DIV': true, 'DL': true, 'DT': true, 'EM': true, 'FONT': true,
 		'H1': true, 'H2': true, 'H3': true, 'H4': true, 'H5': true, 'H6': true, 'HR': true, 'I': true, 'IMG': true, 'LABEL': true, 'LI': true, 'OL': true, 'P': true, 'PRE': true,
 		'SMALL': true, 'SOURCE': true, 'SPAN': true, 'STRONG': true, 'SUB': true, 'SUP': true, 'TABLE': true, 'TBODY': true, 'TR': true, 'TD': true, 'TH': true, 'THEAD': true, 'UL': true, 'U': true, 'VIDEO': true
 	};
 
-	var contentTagWhiteList_ = { 'FORM': true, 'GOOGLE-SHEETS-HTML-ORIGIN': true }; //tags that will be converted to DIVs
+	var _contentTagWhiteList = { 'FORM': true, 'GOOGLE-SHEETS-HTML-ORIGIN': true }; //tags that will be converted to DIVs
 
-	var attributeWhitelist_ = { 'align': true, 'color': true, 'controls': true, 'height': true, 'href': true, 'id': true, 'src': true, 'style': true, 'target': true, 'title': true, 'type': true, 'width': true };
+	var _attributeWhitelist = { 'align': true, 'color': true, 'controls': true, 'height': true, 'href': true, 'id': true, 'src': true, 'style': true, 'target': true, 'title': true, 'type': true, 'width': true };
 
-	var cssWhitelist_ = { 'background-color': true, 'color': true, 'font-size': true, 'font-weight': true, 'text-align': true, 'text-decoration': true, 'width': true };
+	var _cssWhitelist = { 'background-color': true, 'color': true, 'font-size': true, 'font-weight': true, 'text-align': true, 'text-decoration': true, 'width': true };
 
-	var schemaWhiteList_ = [ 'http:', 'https:', 'data:', 'm-files:', 'file:', 'ftp:', 'mailto:', 'pw:' ]; //which "protocols" are allowed in "href", "src" etc
+	var _schemaWhiteList = [ 'http:', 'https:', 'data:', 'm-files:', 'file:', 'ftp:', 'mailto:', 'pw:' ]; //which "protocols" are allowed in "href", "src" etc
 
-	var uriAttributes_ = { 'href': true, 'action': true };
+	var _uriAttributes = { 'href': true, 'action': true };
 
 	var _parser = new DOMParser();
 
@@ -29,44 +29,44 @@ var HtmlSanitizer = new (function () {
 		extraAttributes = (extraAttributes && extraAttributes instanceof Array) ? extraAttributes : [];
 
 		input = input.trim();
-		if (input == "") return ""; //to save performance and not create iframe
+		if (input == "") return ""; //to save performance
 
 		//firefox "bogus node" workaround
 		if (input == "<br>") return "";
 
 		if (input.indexOf("<body")==-1) input = "<body>" + input + "</body>"; //add "body" otherwise some tags are skipped, like <style>
 
-		var iframedoc = _parser.parseFromString(input, "text/html");
+		var doc = _parser.parseFromString(input, "text/html");
 
 		function makeSanitizedCopy(node) {
 			if (node.nodeType == Node.TEXT_NODE) {
 				var newNode = node.cloneNode(true);
-			} else if (node.nodeType == Node.ELEMENT_NODE && (tagWhitelist_[node.tagName] || contentTagWhiteList_[node.tagName] || extraTags.indexOf(node.tagName) > -1)) {
+			} else if (node.nodeType == Node.ELEMENT_NODE && (_tagWhitelist[node.tagName] || _contentTagWhiteList[node.tagName] || extraTags.indexOf(node.tagName) > -1)) {
 
 				//remove useless empty spans (lots of those when pasting from MS Outlook)
 				if ((node.tagName == "SPAN" || node.tagName == "B" || node.tagName == "I" || node.tagName == "U")
 					&& node.innerHTML.trim() == "") {
-					return document.createDocumentFragment();
+					return doc.createDocumentFragment();
 				}
 
-				if (contentTagWhiteList_[node.tagName])
-					newNode = iframedoc.createElement('DIV'); //convert to DIV
+				if (_contentTagWhiteList[node.tagName])
+					newNode = doc.createElement('DIV'); //convert to DIV
 				else
-					newNode = iframedoc.createElement(node.tagName);
+					newNode = doc.createElement(node.tagName);
 
 				for (var i = 0; i < node.attributes.length; i++) {
 					var attr = node.attributes[i];
-					if (attributeWhitelist_[attr.name] || extraAttributes.indexOf(attr.name) > -1) {
+					if (_attributeWhitelist[attr.name] || extraAttributes.indexOf(attr.name) > -1) {
 						if (attr.name == "style") {
 							for (s = 0; s < node.style.length; s++) {
 								var styleName = node.style[s];
-								if (cssWhitelist_[styleName])
+								if (_cssWhitelist[styleName])
 									newNode.style.setProperty(styleName, node.style.getPropertyValue(styleName));
 							}
 						}
 						else {
-							if (uriAttributes_[attr.name]) { //if this is a "uri" attribute, that can have "javascript:" or something
-								if (attr.value.indexOf(":") > -1 && !startsWithAny(attr.value, schemaWhiteList_))
+							if (_uriAttributes[attr.name]) { //if this is a "uri" attribute, that can have "javascript:" or something
+								if (attr.value.indexOf(":") > -1 && !startsWithAny(attr.value, _schemaWhiteList))
 									continue;
 							}
 							newNode.setAttribute(attr.name, attr.value);
@@ -78,12 +78,12 @@ var HtmlSanitizer = new (function () {
 					newNode.appendChild(subCopy, false);
 				}
 			} else {
-				newNode = document.createDocumentFragment();
+				newNode = doc.createDocumentFragment();
 			}
 			return newNode;
 		};
 
-		var resultElement = makeSanitizedCopy(iframedoc.body);
+		var resultElement = makeSanitizedCopy(doc.body);
 		
 		return resultElement.innerHTML
 			.replace(/<br[^>]*>(\S)/g, "<br>\n$1")
@@ -99,8 +99,8 @@ var HtmlSanitizer = new (function () {
 		return false;
 	}
 
-	this.AllowedTags = tagWhitelist_;
-	this.AllowedAttributes = attributeWhitelist_;
-	this.AllowedCssStyles = cssWhitelist_;
-	this.AllowedSchemas = schemaWhiteList_;
+	this.AllowedTags = _tagWhitelist;
+	this.AllowedAttributes = _attributeWhitelist;
+	this.AllowedCssStyles = _cssWhitelist;
+	this.AllowedSchemas = _schemaWhiteList;
 });
