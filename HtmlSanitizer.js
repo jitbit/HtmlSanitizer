73a3ab47ec5b2dfa35973151b8f60c841ae2b0c7
Alexander Yumashev <33555768+alex-jitbit@users.noreply.github.com>
2022-06-10 18:07:47 +0300

    strcit mode, modern vars, more tests

--- HtmlSanitizer.js
+++ HtmlSanitizer.js
@@ -3,26 +3,27 @@
 //homepage https://github.com/jitbit/HtmlSanitizer
 
 //License: MIT https://github.com/jitbit/HtmlSanitizer/blob/master/LICENSE
+'use strict'
 
-var HtmlSanitizer = new (function () {
+const HtmlSanitizer = new (function () {
 
-	var _tagWhitelist = {
+	const _tagWhitelist = {
 		'A': true, 'ABBR': true, 'B': true, 'BLOCKQUOTE': true, 'BODY': true, 'BR': true, 'CENTER': true, 'CODE': true, 'DD': true, 'DIV': true, 'DL': true, 'DT': true, 'EM': true, 'FONT': true,
 		'H1': true, 'H2': true, 'H3': true, 'H4': true, 'H5': true, 'H6': true, 'HR': true, 'I': true, 'IMG': true, 'LABEL': true, 'LI': true, 'OL': true, 'P': true, 'PRE': true,
 		'SMALL': true, 'SOURCE': true, 'SPAN': true, 'STRONG': true, 'SUB': true, 'SUP': true, 'TABLE': true, 'TBODY': true, 'TR': true, 'TD': true, 'TH': true, 'THEAD': true, 'UL': true, 'U': true, 'VIDEO': true
 	};
 
-	var _contentTagWhiteList = { 'FORM': true, 'GOOGLE-SHEETS-HTML-ORIGIN': true }; //tags that will be converted to DIVs
+	const _contentTagWhiteList = { 'FORM': true, 'GOOGLE-SHEETS-HTML-ORIGIN': true }; //tags that will be converted to DIVs
 
-	var _attributeWhitelist = { 'align': true, 'color': true, 'controls': true, 'height': true, 'href': true, 'id': true, 'src': true, 'style': true, 'target': true, 'title': true, 'type': true, 'width': true };
+	const _attributeWhitelist = { 'align': true, 'color': true, 'controls': true, 'height': true, 'href': true, 'id': true, 'src': true, 'style': true, 'target': true, 'title': true, 'type': true, 'width': true };
 
-	var _cssWhitelist = { 'background-color': true, 'color': true, 'font-size': true, 'font-weight': true, 'text-align': true, 'text-decoration': true, 'width': true };
+	const _cssWhitelist = { 'background-color': true, 'color': true, 'font-size': true, 'font-weight': true, 'text-align': true, 'text-decoration': true, 'width': true };
 
-	var _schemaWhiteList = [ 'http:', 'https:', 'data:', 'm-files:', 'file:', 'ftp:', 'mailto:', 'pw:' ]; //which "protocols" are allowed in "href", "src" etc
+	const _schemaWhiteList = [ 'http:', 'https:', 'data:', 'm-files:', 'file:', 'ftp:', 'mailto:', 'pw:' ]; //which "protocols" are allowed in "href", "src" etc
 
-	var _uriAttributes = { 'href': true, 'action': true };
+	const _uriAttributes = { 'href': true, 'action': true };
 
-	var _parser = new DOMParser();
+	const _parser = new DOMParser();
 
 	this.SanitizeHtml = function (input, extraTags, extraAttributes) {
 		extraTags = (extraTags && extraTags instanceof Array) ? extraTags : [];
@@ -31,12 +32,12 @@ var HtmlSanitizer = new (function () {
 		input = input.trim();
 		if (input == "") return ""; //to save performance
 
-		//firefox "bogus node" workaround
+		//firefox "bogus node" workaround for wysiwyg's
 		if (input == "<br>") return "";
 
 		if (input.indexOf("<body")==-1) input = "<body>" + input + "</body>"; //add "body" otherwise some tags are skipped, like <style>
 
-		var doc = _parser.parseFromString(input, "text/html");
+		let doc = _parser.parseFromString(input, "text/html");
 
 		//DOM clobbering check (damn you firefox)
 		if (doc.body.tagName !== 'BODY')
@@ -45,8 +46,9 @@ var HtmlSanitizer = new (function () {
 			doc.createElement.remove();
 
 		function makeSanitizedCopy(node) {
+			let newNode;
 			if (node.nodeType == Node.TEXT_NODE) {
-				var newNode = node.cloneNode(true);
+				newNode = node.cloneNode(true);
 			} else if (node.nodeType == Node.ELEMENT_NODE && (_tagWhitelist[node.tagName] || _contentTagWhiteList[node.tagName] || extraTags.indexOf(node.tagName) > -1)) {
 
 				if (_contentTagWhiteList[node.tagName])
@@ -54,12 +56,12 @@ var HtmlSanitizer = new (function () {
 				else
 					newNode = doc.createElement(node.tagName);
 
-				for (var i = 0; i < node.attributes.length; i++) {
-					var attr = node.attributes[i];
+				for (let i = 0; i < node.attributes.length; i++) {
+					let attr = node.attributes[i];
 					if (_attributeWhitelist[attr.name] || extraAttributes.indexOf(attr.name) > -1) {
 						if (attr.name == "style") {
-							for (s = 0; s < node.style.length; s++) {
-								var styleName = node.style[s];
+							for (let s = 0; s < node.style.length; s++) {
+								let styleName = node.style[s];
 								if (_cssWhitelist[styleName])
 									newNode.style.setProperty(styleName, node.style.getPropertyValue(styleName));
 							}
@@ -73,8 +75,8 @@ var HtmlSanitizer = new (function () {
 						}
 					}
 				}
-				for (i = 0; i < node.childNodes.length; i++) {
-					var subCopy = makeSanitizedCopy(node.childNodes[i]);
+				for (let i = 0; i < node.childNodes.length; i++) {
+					let subCopy = makeSanitizedCopy(node.childNodes[i]);
 					newNode.appendChild(subCopy, false);
 				}
 
@@ -90,7 +92,7 @@ var HtmlSanitizer = new (function () {
 			return newNode;
 		};
 
-		var resultElement = makeSanitizedCopy(doc.body);
+		let resultElement = makeSanitizedCopy(doc.body);
 		
 		return resultElement.innerHTML
 			.replace(/<br[^>]*>(\S)/g, "<br>\n$1")
@@ -98,7 +100,7 @@ var HtmlSanitizer = new (function () {
 	}
 
 	function startsWithAny(str, substrings) {
-		for (var i = 0; i < substrings.length; i++) {
+		for (let i = 0; i < substrings.length; i++) {
 			if (str.indexOf(substrings[i]) == 0) {
 				return true;
 			}
